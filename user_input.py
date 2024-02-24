c48dbe0684be46a2f8d8ec12fc218f89fcabfa70
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2024-02-24 21:04:17 +0300

    Python week one assignment

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+age = input("How  old are you? ")
+location = input("Where do you live? ")
+print(f"Your information: \nName: {name} \nAge: {age} \nLocation: {location}")
