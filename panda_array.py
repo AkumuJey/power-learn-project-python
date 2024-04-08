a42f5adb47e625dfc0bb55e9eb9b38509cd25306
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2024-04-08 16:24:42 +0300

    Pandas and matplotlib for data analysis

--- panda_array.py
+++ panda_array.py
@@ -1,7 +1,30 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 data = {"name": ["Akumu", "Joseph", "Bob"], "age" : [23, 56, 89], "city": ["Rangwe", "Oyugis", "Homa Bay"]}
 df = pd.DataFrame(data)
+
+food = pd.read_csv("onlinefoods.csv")
+column_names = ['Age', 'Gender', 'Marital Status', 'Occupation', 'Monthly Income']
+
+food_df = food.columns
+new_df = food[column_names]
+
+selected_column = 'Age'
+grouped_data = food[selected_column].value_counts().sort_index()
+
+plt.figure(figsize=(10, 6))
+grouped_data.plot(kind='bar', color='skyblue')
+plt.title(f'Bar Chart for {selected_column}')
+plt.xlabel(selected_column)
+plt.ylabel('Count')
+plt.xticks(rotation=45)  # Rotate x-axis labels for better visibility
+plt.grid(axis='y')  # Add gridlines on y-axis
+plt.tight_layout()
+plt.show()
+
+print(len(new_df))
+print(food_df)
 
 print(df)
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,17 +1,4 @@
-def findEvenNumbers(arr):
-#     even = []
-#     for num in arr:
-#         if num % 2 == 0:
-#             even.append(num)
-#     return even
-
-# print(findEvenNumbers([4, 6, 7, 8, 8, 13, 53, 31]))
-
-# def sumOfNumbers(*nums):
-#     total = 0
-#     for n in nums:
-#         total += n
-#     return total
-
-# print(sumOfNumbers(3,4,5,5,8,9))
-# print(sumOfNumbers(3,4,5,8, 556,66))
+te x-axis labels for better visibility
+plt.grid(axis='y')  # Add gridlines on y-axis
+plt.tight_layout()
+plt.show()
