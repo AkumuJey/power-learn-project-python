010ae86bb1e8531528309faae6f990a4b942a00c
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2024-04-08 15:59:56 +0300

    Working with pandas dataframes

--- array_package.py
+++ array_package.py
@@ -2,6 +2,9 @@ import numpy as np
 
 a = np.array([3, 5, 6, 7, 8])
 b = np. array([6, 7, 12, 45, 71])
+c = np.array([2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+print(c.reshape(3,3))
 
 print(a*b)
 print(a-b)
---
+++ panda_array.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+data = {"name": ["Akumu", "Joseph", "Bob"], "age" : [23, 56, 89], "city": ["Rangwe", "Oyugis", "Homa Bay"]}
+df = pd.DataFrame(data)
+
+print(df)
+
